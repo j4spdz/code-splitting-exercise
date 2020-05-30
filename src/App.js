f85f6259d567b5da4d0a7bff9d38a8ca3d8b3357
j4spdz <code.jack4spadesz@gmail.com>
2020-05-30 10:40:35 +0800

    Added suspense for fallback

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, Suspense } from 'react';
 import './App.css';
-import loadable from '@loadable/component'
+import { lazy } from '@loadable/component'
 
 const App = () => {
   const [ route, setRoute ] = useState('page1');
 
   const onRouteChange = (route) => setRoute(route);
-  const AsyncPage = loadable(props => import(`./Components/${props.page}`));
+  const AsyncPage = lazy(props => import(`./Components/${props.page}`));
 
-  if (route === 'page1') {
-    return <AsyncPage page="Page1" onRouteChange={onRouteChange}/>
-  } else if (route === 'page2') {
-    return <AsyncPage page="Page2" onRouteChange={onRouteChange}/>
-  } else {
-    return <AsyncPage page="Page3" onRouteChange={onRouteChange}/>
+  const RenderPage = () => {
+    if (route === 'page1') {
+      return <AsyncPage page="Page1" onRouteChange={onRouteChange}/>
+    } else if (route === 'page2') {
+      return <AsyncPage page="Page2" onRouteChange={onRouteChange}/>
+    } else {
+      return <AsyncPage page="Page3" onRouteChange={onRouteChange}/>
+    }   
   }
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <RenderPage />
+    </Suspense>
+  )
 }
 
 export default App;
